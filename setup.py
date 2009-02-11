daa0a7c05c093ac99529f7bb153f55ea519d88bc
Brian Rosner <brosner@gmail.com>
2009-02-11 11:22:29 -0700

    Added a setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+    name = "django-private-parts",
+    version = "0.1pre",
+    author = "Brian Rosner",
+    author_email = "brosner@gmail.com",
+    description = "A Django reusable app to deal with private parts of a website based on user preference.",
+    long_description = open("README").read(),
+    url = "http://github.com/brosner/django-private-parts/",
+    license = "MIT",
+)
